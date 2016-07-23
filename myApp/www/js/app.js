a5b1aa31400795c7b49b42ac0f0d08cdd6cf98aa
Pandelis Zembashis <info@pandelis.me>
2016-07-23 05:49:05 +0100

    bunch of interface stuff to the main screen

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -67,7 +67,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
+  })
+
+  .state('app.home', {
+      url:'/home',
+      views:{
+        'menuContent': {
+          templateUrl: 'templates/homeScreen.html'
+        }
+      }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
